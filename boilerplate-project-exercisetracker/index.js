da455ccce039da2297c52291a1f7027475b29c1d
dawit2123 <dawitzewdu2123@gmail.com>
2024-05-02 21:34:35 +0300

    project done

--- boilerplate-project-exercisetracker/index.js
+++ boilerplate-project-exercisetracker/index.js
@@ -2,123 +2,110 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const ObjectId = require("mongoose").Types.ObjectId;
+const { Schema } = mongoose;
 const bodyParser = require("body-parser");
+require("dotenv").config();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
-//connect to the mongodb
-const connectToMongoDB = async () =>
-  await mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => {
-      console.log("Error is " + err);
-    });
-connectToMongoDB();
-//userSchema
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI);
+}
+
+const userSchema = new Schema(
+  {
+    username: { type: String, require: true, unique: true },
+    exercises: [
+      {
+        description: String,
+        duration: Number,
+        date: Date,
+      },
+    ],
   },
-});
+  { versionKey: false }
+);
+
 const User = mongoose.model("User", userSchema);
+const ERROR = { error: "There was an error while getting the users." };
 
-// exercise Schema
-const exerciseSchema = new mongoose.Schema({
-  id: {
-    type: String,
-    required: true,
-    unique: false,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  duration: {
-    type: Number,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now(),
-  },
-});
-const Exercise = mongoose.model("Exercise", exerciseSchema);
-
-//post method to the /api/users
-app.post("/api/users", async (req, res, next) => {
-  let username = req.body.username;
-  const user = await User.create({ username: username });
-  res.json({
-    username: user.username,
-    _id: user._id,
-  });
-});
-
-//get request ot the /api/users
-app.get("/api/users", async (req, res, next) => {
+app.get("/api/users", async (req, res) => {
   const data = await User.find({});
-  res.json({
-    users: data,
-  });
+  res.json(data);
 });
 
-//post request to the /api/users/:_id/exercises
-app.post("/api/users/:_id/exercises", async (req, res, next) => {
-  const data = await Exercise.create({
-    id: req.params._id,
-    description: req.body.description,
-    duration: req.body.duration,
-    date: new Date(req.body.date),
-  });
-  const userData = await User.findById(req.params._id);
-  console.log("user data is " + userData);
-  res.json({
-    username: userData.username,
-    description: data.description,
-    duration: data.duration,
-    date: data.date.toDateString(),
-    _id: data._id,
-  });
-});
+app.get("/api/users/:id/logs", async (req, res) => {
+  const id = req.params.id;
+  const dateFrom = new Date(req.query.from);
+  const dateTo = new Date(req.query.to);
+  const limit = parseInt(req.query.limit);
 
-//get request to the /api/users/:_id/logs endpoint
-app.get("/api/users/:_id/logs", async (req, res, next) => {
-  const fromDate = new Date(req.query.from);
-  const toDate = new Date(req.query.to);
-  const limit = req.query.limit;
-  const data = await Exercise.find({ id: req.params._id });
-  console.log("The data is " + data);
-  let log = data;
-  console.log(fromDate + "and" + toDate + "and" + limit);
-  if (fromDate && toDate) {
-    console.log("from date");
-    let logTemp = data.filter((exercise) => {
-      exercise.date > fromDate && exercise.date < toDate;
+  const data = await User.findOne({ _id: new ObjectId(id) });
+  let log = [];
+
+  data.exercises.filter(
+    (exercise) =>
+      new Date(Date.parse(exercise.date)).getTime() > dateFrom &&
+      new Date(Date.parse(exercise.date)).getTime() < dateTo
+  );
+
+  for (const exercise of data.exercises) {
+    log.push({
+      description: exercise.description,
+      duration: exercise.duration,
+      date: new Date(exercise.date).toDateString(),
     });
-    log = logTemp;
   }
-  if (limit) {
-    console.log("limit");
-    log = log.slice(0, limit);
-  }
-  let logData = log.map((val) => {
-    val.date = val.date.toDateString();
-  });
+
+  if (limit) log = log.slice(0, limit);
+
   res.json({
-    count: data.length,
-    log: logData,
     _id: data._id,
+    username: data.username,
+    count: log.length,
+    log: log,
   });
+});
+
+app.post("/api/users", async (req, res) => {
+  const username = req.body.username;
+  const data = await User.create({ username: username });
+  res.json({ _id: data._id, username: data.username });
+});
+
+app.post("/api/users/:id/exercises", async (req, res) => {
+  const id = req.params.id;
+  let { description, duration, date } = req.body;
+
+  const newExercise = {
+    description: description,
+    duration: duration,
+    date: date ? new Date(date).toDateString() : new Date().toDateString(),
+  };
+
+  const data = await User.findOne({ _id: new ObjectId(id) });
+  data.exercises.push(newExercise);
+  const result = await data.save();
+  const response = {
+    username: result.username,
+    description: result.exercises[result.exercises.length - 1].description,
+    duration: result.exercises[result.exercises.length - 1].duration,
+    date: new Date(
+      result.exercises[result.exercises.length - 1].date
+    ).toDateString(),
+    _id: result._id,
+  };
+
+  res.json(response);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
